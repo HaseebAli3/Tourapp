6764e5e67cf956e339b2b7c13a6f881b2a5dc5ee
M Haseeb Ali <astudent622@gmail.com>
2025-06-10 16:13:39 +0500

    Updated model file

--- accounts/models.py
+++ accounts/models.py
@@ -3,14 +3,11 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 
 class UserManager(BaseUserManager):
     def create_user(self, email, full_name, password=None, **extra_fields):
-        if not email:
-            raise ValueError('Users must have an email address')
-        
+       
         email = self.normalize_email(email)
         user = self.model(email=email, full_name=full_name, **extra_fields)
         user.set_password(password)
-        user.save(using=self._db)
-        return user
+        user.save()
 
     def create_superuser(self, email, full_name, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
