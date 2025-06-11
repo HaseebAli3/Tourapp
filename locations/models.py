75176b4df1be7f143ce68ec8d28f4bb116e88633
M Haseeb Ali <astudent622@gmail.com>
2025-06-11 11:24:33 +0500

    Location App:worked on location app convert view simple to generic make the price field mendatory

--- locations/models.py
+++ locations/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 class Location(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
     location_url = models.URLField(blank=True, null=True)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- locations/views.py
+++ locations/views.py
@@ -1,18 +1,16 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
 from .models import Location
 from .serializers import LocationSerializer
 
-class LocationListView(APIView):
-    def get(self, request):
-        locations = Location.objects.all()
-        serializer = LocationSerializer(locations, many=True)
-        return Response(serializer.data)
+# 1. List all Locations
+class LocationListView(generics.ListAPIView):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
 
-class LocationByNameView(APIView):
-    def get(self, request, name):
-        locations = Location.objects.filter(name__icontains=name)
-        if not locations:
-            return Response({"error": "No locations found"}, status=404)
-        serializer = LocationSerializer(locations, many=True)
-        return Response(serializer.data)
+# 2. List Locations by Name (custom filter with generic view)
+class LocationByNameView(generics.ListAPIView):
+    serializer_class = LocationSerializer
+
+    def get_queryset(self):
+        name = self.kwargs['name']
+        return Location.objects.filter(name__icontains=name)
