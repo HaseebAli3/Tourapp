d09b96d335741b9c6a52757973a60a409eabf13f
M Haseeb Ali <astudent622@gmail.com>
2025-06-11 15:04:09 +0500

    Location App:Update the admin file

--- booking/admin.py
+++ booking/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Booking
 
-admin.site.register(Booking)
+
 # Register your models here.
+class bookingAdmin(admin.ModelAdmin):
+    # Fields to show in the admin form
+    list_display = ('user', 'location', 'booking_date')
+admin.site.register(Booking, bookingAdmin)
